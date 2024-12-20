e392bf3b0c64d937efc77823390d6bedcefd4c16
Daniel Irons <danielirons@protonmail.com>
2024-12-20 17:49:04 +0000

    added constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const statValues = [-3, -2, -1, 0, 1, 2, 3];
+
+export const stats = [
+	{ label: 'Agility', key: 'agility' },
+	{ label: 'Presence', key: 'presence' },
+	{ label: 'Strength', key: 'strength' },
+	{ label: 'Toughness', key: 'toughness' }
+];
+
+export const STORAGE_KEY = 'warband_data';
